0995f51421f340db564b631c9ef4528a24bf3d19
Lungelo Sibisi <lungelosibisi83@gmail.com>
2025-11-11 08:17:48 +0200

    Update campaigns.ts

--- src/data/campaigns.ts
+++ src/data/campaigns.ts
@@ -123,13 +123,6 @@ export const campaigns: Campaign[] = [
       },
       {
         type: 'image',
-        src: '/assets/campaigns/mazda-gfv-q3-2025/Mazda2 Retail.jpg',
-        alt: 'Mazda2 headline frame',
-        width: 1080,
-        height: 1920
-      },
-      {
-        type: 'image',
         src: '/assets/campaigns/mazda-gfv-q3-2025/Mazda2 Retail2.jpg',
         alt: 'Mazda2 headline frame',
         width: 1080,
