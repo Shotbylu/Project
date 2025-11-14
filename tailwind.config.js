ceb18792e5338b3d4e6f228efcaa3fc96e99d2c8
Lungelo Sibisi <lungelosibisi83@gmail.com>
2025-11-14 06:58:29 +0200

    Replace webfonts with system UI font stack

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Helvetica', '"Helvetica Neue"', 'Arial', ...defaultTheme.fontFamily.sans],
+        sans: ['var(--font-sans)', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        12: 'var(--fs-12)',
+        14: 'var(--fs-14)',
+        16: 'var(--fs-16)',
+        20: 'var(--fs-20)',
+        24: 'var(--fs-24)',
+        32: 'var(--fs-32)',
+        48: 'var(--fs-48)',
       },
     },
   },
